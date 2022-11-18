87479233c47e8d225b2e6bcaa2ea2d8f3a77911c
Drake.Mohit <drake.mohit@rubicotech.in>
2022-11-18 23:07:20 +0530

    init: server config

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import './config/env.config';
+import express from 'express';
+
+class Server {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+  }
+
+  public config(): void {
+    this.app.set('port', process.env.PORT);
+  }
+
+  public start(): void {
+    this.app.listen(this.app.get('port'), () => {
+      console.log(
+        'Server is running at http://localhost:%d',
+        this.app.get('port')
+      );
+    });
+  }
+}
+
+const server = new Server();
+server.start();
